05864513680f2567b86d6f1f0281b576b66563e0
eks <37926645+iRedScarf@users.noreply.github.com>
2024-02-02 20:31:56 +0800

    更新 WCCI+.py

--- WCCI+.py
+++ WCCI+.py
@@ -16,23 +16,13 @@ def handle_data(param):
 
     TYP = NumericSeries("WCCI")
     TYP[-1] = (h[-1] + l[-1] + c[-1]) / 3
-    TYP[-2] = (h[-2] + l[-2] + c[-2]) / 3
 
-    if TYP[-1] is not None and TYP[-2] is not None:
+    if TYP[-1] is not None:
         Avg = SMA(TYP, n, Weight=2)
         AvgDev = AvgDeviation(TYP, n)
         if Avg is not None and AvgDev is not None and AvgDev != 0:
-            WCCI_1 = (TYP[-1] - Avg) * 10000 / (m * AvgDev)
-            WCCI_2 = (TYP[-2] - Avg) * 10000 / (m * AvgDev)
+            WCCI = (TYP[-1] - Avg) * 10000 / (m * AvgDev)
         else:
-            WCCI_1 = None
-            WCCI_2 = None
+            WCCI = None
     else:
-        WCCI_1 = None
-        WCCI_2 = None
-
-    if WCCI_1 is not None and WCCI_2 is not None:
-        if WCCI_2 >=100 and WCCI_1 < 100:
-            PlotDot("WCCI", h[-1], 2, ColorDown())
-        if WCCI_2 <= -100 and WCCI_1 > -100:
-            PlotDot("WCCI", l[-1], 2, ColorUp())
+        WCCI = None
